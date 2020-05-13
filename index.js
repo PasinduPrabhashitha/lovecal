8482ab23a70f7b975572a741ed8a77947c1fed71
PasinduPrabhashitha <63661350+PasinduPrabhashitha@users.noreply.github.com>
2020-05-13 16:34:10 +0530

    Love cal 1.0

---
+++ index.js
@@ -0,0 +1,19 @@
+function names(){
+    var randomNumber = Math.floor(Math.random() * 100)+1
+     var yourName = document.getElementById("firstname").value
+     var crushName = document.getElementById("secondname").value
+
+     var element = document.getElementById("love-text")
+
+     if (randomNumber >= 70) {
+       alert("Love between " + yourName + " and " + crushName + " is " + randomNumber + "%" + " Your head over heels.");
+     }else if (randomNumber >= 30) {
+       alert("Love between " + yourName + " and " + crushName + " is " + randomNumber + "%" + " You have a match made in heaven.");
+     }else{
+        alert("Love between " + yourName + " and " + crushName + " is " + randomNumber + "%" + " But don't worry.");
+     }
+
+
+
+
+  }
